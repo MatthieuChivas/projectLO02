37a792a98762592ff880370a34b3ee7631e60a76
MatthieuChivas <117097696+MatthieuChivas@users.noreply.github.com>
2022-12-06 16:31:52 +0100

    Update ZoneInfluence.java

--- src/cestdubrutal/ZoneInfluence.java
+++ src/cestdubrutal/ZoneInfluence.java
@@ -1,10 +1,7 @@
 package cestdubrutal;
 
 public class ZoneInfluence {
-	private String nom = new String();
-	private String nombreEtudiant = new String();
-	private boolean controle;
-	public int nbJoueur;
+	
 	
 	public void consulterECTS() {
 		
