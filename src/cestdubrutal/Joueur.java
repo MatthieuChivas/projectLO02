998c88b40469611aa2b4fc965b83c4a647efa4e1
MathisMidelet <117585853+MathisMidelet@users.noreply.github.com>
2022-12-15 19:57:49 +0100

    Update Joueur.java

--- src/cestdubrutal/Joueur.java
+++ src/cestdubrutal/Joueur.java
@@ -1,6 +1,8 @@
 package cestdubrutal;
 
 import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
 
 import cestdubrutal.Joueur.Programme;
 
@@ -14,71 +16,106 @@ public class Joueur {
 	//Attributs
 	private String pseudo;
 	private Programme programme;
-	private Eleve[] tabEleve;
+	private ArrayList<Eleve> tabEleve= new ArrayList(20);
 	
 	Scanner sc = new Scanner(System.in);
 	
 	//Constructeur
 	public Joueur() {		
-//		System.out.println("Mettre le pseudo du joueur:");
-//		this.pseudo = sc.nextLine();
-//		
-//		
-//		System.out.println("Renseigner le programme du joueur (1 pour ISI, 2 pour RT...");
-//		String p=sc.nextLine();
-//		
-//		
-//		switch(p) {
-//		case "1":
-//			this.programme= Programme.ISI;
-//		case "2":
-//			this.programme= Programme.RT;
-//		case "3":
-//			this.programme= Programme.A2I;
-//		case "4":
-//			this.programme= Programme.GI;
-//		default:
-//			this.programme= Programme.ISI;
-//			//à continuer	
-//		}
+		System.out.println("Mettre votre pseudo:");
+		this.pseudo = sc.nextLine();
+		
+		
+		System.out.println(pseudo + " Renseigner votre programme");
+		int p=8;// entré dans la boucle while
+		while(p<=0 || p>=8) {
+			System.out.println("ISI:1 RT:2 A2I:3 GI:4 GM:5 MTE:6 MM:7");
+			int p1=sc.nextInt();
+			if(p1<=0 || p1>=8) {
+				System.out.println("erreur");
+			}
+			else {
+				if(p1==1) {
+					this.programme= Programme.ISI;
+					p=p1;
+				}
+				if(p1==2) {
+					this.programme= Programme.RT;
+					p=p1;
+				}
+				if(p1==3) {
+					this.programme= Programme.A2I;
+					p=p1;
+				}
+				if(p1==4) {
+					this.programme= Programme.GI;
+					p=p1;
+				}
+				if(p1==5) {
+					this.programme= Programme.GM;
+					p=p1;
+				}
+				if(p1==6) {
+					this.programme= Programme.MTE;
+					p=p1;
+				}
+				if(p1==7) {
+					this.programme= Programme.MM;
+					p=p1;
+				}
+			}
+			
+		}
+				
+		System.out.println("Vous avez choisi "+programme);	
 		
 		//création des eleves associé à un joueur
-		this.setTabEleve(new Eleve[20]);
 		for(int i=0;i<20;i++) {
-			this.getTabEleve()[i]=new Eleve();
+			this.tabEleve.add(new Eleve());
 		}
 		
-		
-		//attribuer point direct (changer valeur borne car sinon chiant i<20)
 		//construction des élèves
-		for(int i=0;i<2;i++) {
-//			System.out.println("int force, int dexterite, int resistance, int constitution, int initiative");
-//			int force=sc.nextInt();
-//			int dexterite=sc.nextInt();
-//			int resistance=sc.nextInt();
-//			int constitution = sc.nextInt();
-//			int initiave= sc.nextInt();
-//			this.getTabEleve()[i].CharacteristiqueEleve.attribuerPoints(force, dexterite, resistance, constitution, initiave);
-
-			this.getTabEleve()[i].CharacteristiqueEleve.attribuerPoints(10, 10, 10, 10, 10);
-			
-			//choix strat
-			System.out.println("Choix Strat 1: Attaque 2: Defense 3: Aléatoire");
-			int choix = sc.nextInt();
-			switch(choix) {
-			case 1:
-				this.getTabEleve()[i].setStrat(Eleve.Strategie.Attaquer);
-				break;
-			case 2:
-				this.getTabEleve()[i].setStrat(Eleve.Strategie.Soigner);
-				break;
-			case 3:
-				this.getTabEleve()[i].setStrat(Eleve.Strategie.Aleatoire);
-				break;
+		//pour l'instant tout est automatique
+		for(int i=0; i<=14; i++) {
+			this.tabEleve.get(i).CharacteristiqueEleve.attribuerPoints(0, 0, 0, 0, 0);
+			this.tabEleve.get(i).setStrat(Eleve.Strategie.Aleatoire);
+			if(tabEleve.get(i).Strat == Eleve.Strategie.Aleatoire) {
+				double x = Math.random();
+				if(x<=0.5) {
+					this.tabEleve.get(i).setStrat(Eleve.Strategie.Attaquer);
+				}
+				else {
+					this.tabEleve.get(i).setStrat(Eleve.Strategie.Soigner);
+				}
 			}
-			//ajouter un aléatoire avec une fonction de Eleve si aléatoire...
-			
+			System.out.println("l'etudiant (" + i + ") a comme strategie: " + tabEleve.get(i).Strat);
 		}
+		for(int i=15; i<=18; i++) {
+			this.tabEleve.get(i).CharacteristiqueEleve.attribuerPoints(1, 1, 1, 1, 1);
+			this.tabEleve.get(i).setStrat(Eleve.Strategie.Aleatoire);
+			if(tabEleve.get(i).Strat == Eleve.Strategie.Aleatoire) {
+				double x = Math.random();
+				if(x<=0.5) {
+					this.tabEleve.get(i).setStrat(Eleve.Strategie.Attaquer);
+				}
+				else {
+					this.tabEleve.get(i).setStrat(Eleve.Strategie.Soigner);
+				}
+			}
+			System.out.println("l'etudiant d'elite (" + i + ") a comme strategie: " + tabEleve.get(i).Strat);
+		}
+		this.tabEleve.get(19).CharacteristiqueEleve.attribuerPoints(2, 2, 2, 10, 2);
+		this.tabEleve.get(19).setStrat(Eleve.Strategie.Aleatoire);
+		if(tabEleve.get(19).Strat == Eleve.Strategie.Aleatoire) {
+			double x = Math.random();
+			if(x<=0.5) {
+				this.tabEleve.get(19).setStrat(Eleve.Strategie.Attaquer);
+			}
+			else {
+				this.tabEleve.get(19).setStrat(Eleve.Strategie.Soigner);
+			}
+		}
+		System.out.println("maitre du gobi (19) a comme strategie: " + tabEleve.get(19).Strat);
 	}
 	
 	public void choixReserviste() {
@@ -102,11 +139,15 @@ public class Joueur {
 		System.exit(1);
 	}
 
-	public Eleve[] getTabEleve() {
+	public ArrayList<Eleve> getTabEleve() {
 		return tabEleve;
 	}
+	
+	public Eleve getTabEleve(int e) {
+		return tabEleve.get(e);
+	}
 
-	public void setTabEleve(Eleve[] tabEleve) {
+	public void setTabEleve(ArrayList<Eleve> tabEleve) {
 		this.tabEleve = tabEleve;
 	}
 
